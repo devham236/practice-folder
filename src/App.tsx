80d7fd4557baf3316702e9ad6f368f9e38919123
Hamza Mirza <hamza665@hotmail.de>
2025-01-23 12:55:23 +0100

    set up basic structure/functionality for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Compare from "./pages/Compare";
+import Locations from "./pages/Locations";
+import Impressum from "./pages/Impressum";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Compare />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/impressum" element={<Impressum />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/pages/Impressum.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Impressum = () => {
+  return <div></div>;
+};
+
+export default Impressum;
